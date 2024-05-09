b27b5ceca565b04240372dda0c9ab03b1d6fdf39
Antonio2015 <apaivapy@gmail.com>
2024-05-09 16:14:47 -0400

    Configuración path

--- src/main/java/com/web/ecommerce/service/UploadFileService.java
+++ src/main/java/com/web/ecommerce/service/UploadFileService.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class UploadFileService {
 	//private String folder="images//";
-	String directorio = System.getProperty("user.dir")+"\\images";
+	String directorio = System.getProperty("user.dir")+"//images";
 	private String folder=directorio+"//";
 	
 	public String saveImage(MultipartFile file) throws IOException {
